nimisolo/scanmem

commit a3641fc9efd797e3cc1fd605520d2871011183fe

gui: Fix bytes-to-string conversion for py2&3
Python 2 and 3 have vastly different models for strings. So introduce
a couple of encode/decode functions based on Python 3 behavior to
abstract away the difference.

Fixes #172 and #173 for both Python 2 and 3

gui/GameConqueror.py
@@ -41,7 +41,6 @@ from gi.repository import GObject
 from consts import *
 from hexview import HexView
 from backend import GameConquerorBackend
-from misc import u
 import misc
 
 import locale
@@ -778,7 +777,7 @@ class GameConqueror():
         elif typename == 'bytearray':
             return (len(value.strip())+1)/3
         elif typename == 'string':
-            return len(value)
+            return len(misc.encode(value))
         return None
 
     # parse bytes dumped by scanmem into number, string, etc.
@@ -788,9 +787,9 @@ class GameConqueror():
         if typename in TYPENAMES_G2STRUCT:
             return struct.unpack(TYPENAMES_G2STRUCT[typename], databytes)[0]
         elif typename == 'string':
-            return str(u(databytes))
+            return misc.decode(databytes, 'replace')
         elif typename == 'bytearray':
-            databytes = u(databytes)
+            databytes = misc.decode(databytes)
             return ' '.join(['%02x'%ord(i) for i in databytes])
         else:
             return databytes
@@ -847,7 +846,7 @@ class GameConqueror():
                 self.show_error(_('Cannot read memory'))
                 return
             self.last_hexedit_address = (start_addr, end_addr)
-            self.memoryeditor_hexview.payload = u(data)
+            self.memoryeditor_hexview.payload = misc.u(data)
             self.memoryeditor_hexview.base_addr = start_addr
         
         # set editable flag
@@ -1034,7 +1033,7 @@ class GameConqueror():
             # temporarily disable model for scanresult_liststore for the sake of performance
             self.scanresult_liststore.clear()
             for line in lines:
-                line = str(u(line))
+                line = misc.decode(line)
                 (mid, line) = line.split(']', 1)
                 mid = int(mid.strip(' []'))
                 (addr, off, rt, val, t) = list(map(str.strip, line.split(',')[:5]))

gui/backend.py
@@ -21,7 +21,8 @@ import sys
 import os
 import ctypes
 import tempfile
-from misc import u
+
+import misc
 
 class GameConquerorBackend():
     BACKEND_FUNCS = {
@@ -55,20 +56,21 @@ class GameConquerorBackend():
                 backup_stdout_fileno = os.dup(sys.stdout.fileno())
                 os.dup2(directed_file.fileno(), sys.stdout.fileno())
 
-                self.lib.backend_exec_cmd(ctypes.c_char_p(cmd.encode()))
+                self.lib.backend_exec_cmd(ctypes.c_char_p(misc.encode(cmd)))
 
                 os.dup2(backup_stdout_fileno, sys.stdout.fileno())
                 os.close(backup_stdout_fileno)
                 directed_file.seek(0)
                 return directed_file.readlines()
         else:
-            self.lib.backend_exec_cmd(ctypes.c_char_p(cmd.encode()))
+
+            self.lib.backend_exec_cmd(ctypes.c_char_p(misc.encode(cmd)))
 
     def get_match_count(self):
         return self.lib.get_num_matches()
 
     def get_version(self):
-        return u(self.lib.get_version())
+        return misc.decode(self.lib.get_version())
 
     def get_scan_progress(self):
         return self.lib.get_scan_progress()

gui/misc.py
@@ -206,3 +206,18 @@ def u(x):
         return codecs.unicode_escape_decode(x)[0]
     else:
         return x
+
+# Interface for bytes<>string conversion for py2/3
+# Usage is the same you'd do in py3, call `decode` on external raw data
+# and `encode` to work with the memory representation
+def decode(raw_bytes, errors='strict'):
+    if PY3K:
+        return raw_bytes.decode(errors=errors)
+    else:
+        return str(raw_bytes)
+
+def encode(unicode_string, errors='strict'):
+    if PY3K:
+        return unicode_string.encode(errors=errors)
+    else:
+        return unicode_string

commit 4b2df7e6dc5cacd2b2817b7258b6a8e29cec529a

gui: Fix bytearray updating in python3
Bytearrays are erroneously passed through a 'decode' step, which
interprets certain byte sequences as unicode multichars.

Use instead the new str2bytes function to convert to an integers list.

Fixes: a3641fc

gui/GameConqueror.py
@@ -789,8 +789,8 @@ class GameConqueror():
         elif typename == 'string':
             return misc.decode(databytes, 'replace')
         elif typename == 'bytearray':
-            databytes = misc.decode(databytes)
-            return ' '.join(['%02x'%ord(i) for i in databytes])
+            databytes = misc.str2bytes(databytes)
+            return ' '.join(['%02x' %(i,) for i in databytes])
         else:
             return databytes
 